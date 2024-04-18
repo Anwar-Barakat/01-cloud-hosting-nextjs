7b06d57407b704bb0835ec6e91a988782d3bd2ba
Anwar-Barakat <brkatanwar0@gmail.com>
2024-04-18 21:49:45 +0400

    build the projects pages

---
+++ src/app/articles/search/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ArticleSearchPage = () => {
+    return (
+        <div>
+            Article Search Page
+        </div>
+    )
+}
+
+export default ArticleSearchPage
